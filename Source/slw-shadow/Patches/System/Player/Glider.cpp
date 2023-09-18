18978a102a49d08282987316c55d0f921b08fe11
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-18 17:59:42 +0200

    Update Glider Booster functions

--- Source/slw-shadow/Patches/System/Player/Glider.cpp
+++ Source/slw-shadow/Patches/System/Player/Glider.cpp
@@ -1,14 +1,41 @@
 #include "pch.h"
 #include "Glider.h"
 
+void slw_shadow::Player::CGlider::CreateGliderBoosters()
+{
+    auto* pVisualGoc = GetComponent<app::fnd::GOCVisualModel>();
+    auto* pEffectGoc = GetComponent<app::game::GOCEffect>();
+
+	if (!pVisualGoc || !pEffectGoc)
+		return;
+
+	app::game::EffectCreateInfo effectInfo{};
+	effectInfo.m_pName = "ef_dl1_stagefire_lp";
+	effectInfo.m_Unk1 = 1.0f;
+	effectInfo.m_Unk2 = 1;
+	effectInfo.m_Unk3 = true;
+	effectInfo.m_pVisual = pVisualGoc;
+
+	effectInfo.m_Position = { 0.0f, 0.0f, -3.0f };
+	effectInfo.m_Rotation = { 0.707f, 0, 0, -0.707f };
+
+	// Create Left Booster
+	effectInfo.m_pBoneName = "pBoost_L";
+	pEffectGoc->CreateEffectLoopEx(&GliderBoosterEffects[0], effectInfo);
+
+	// Create Right Booster
+	effectInfo.m_pBoneName = "pBoost_R";
+	pEffectGoc->CreateEffectLoopEx(&GliderBoosterEffects[1], effectInfo);
+
+	for (size_t i = 0; i < 2; i++)
+		GliderBoosterEffects[i].SetGlobalScaling(0.65f);
+}
+
 HOOK(void, __fastcall, AddCallbackHook, ASLR(0x008EB820), slw_shadow::Player::CGlider* in_pThis, void* edx, app::GameDocument& in_rDocument)
 {
     originalAddCallbackHook(in_pThis, edx, in_rDocument);
 
-    auto* pVisualGoc = in_pThis->GetComponent<app::fnd::GOCVisualModel>();
-    auto* pEffectGoc = in_pThis->GetComponent<app::game::GOCEffect>();
-
-    app::Player::CreateGliderBoosters(pVisualGoc, pEffectGoc);
+    in_pThis->CreateGliderBoosters();
 }
 
 void slw_shadow::Player::CGlider::InstallPatches()
--- Source/slw-shadow/Patches/System/Player/Glider.h
+++ Source/slw-shadow/Patches/System/Player/Glider.h
@@ -4,7 +4,14 @@ namespace slw_shadow::Player
 {
 	class CGlider : public app::Player::CPlayerVehicle
 	{
+	private:
+		inline static const size_t ms_GliderBoosterEffectCount = 10;
+
 	public:
+		app::Effect::CEffectHandle<> GliderBoosterEffects[ms_GliderBoosterEffectCount]{};
+		
+		void CreateGliderBoosters();
+
 		static void InstallPatches();
 		static void InstallHooks();
 	};
